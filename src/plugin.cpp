6f01238eb59e8201abf6d99b878056c28d505552
rodyherrera <contact@rodyherrera.com>
2026-06-05 01:08:42 -0400

    refactor: simplify crystal structure option binding in plugin configuration

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,18 +1,12 @@
 #include <volt/plugin/plugin_entry.h>
 #include <volt/analysis/cna_service.h>
-#include <volt/structures/crystal_structure_types.h>
 
 using namespace Volt;
 using namespace Volt::Plugin;
 using S = CommonNeighborAnalysisService;
 
 static const std::vector<OptionBinding<S>> bindings = {
-    opt<S>({"--crystal_structure", "string", "FCC|BCC|HCP|CUBIC_DIAMOND|HEX_DIAMOND", "FCC"},
-        [](S& s, const OptsMap& opts) {
-            LatticeStructureType t = LATTICE_FCC;
-            parseLatticeStructureType(CLI::getString(opts, "--crystal_structure", "FCC"), t);
-            s.setInputCrystalStructure(t);
-        }),
+    optLattice("--crystal_structure", "FCC|BCC|HCP|CUBIC_DIAMOND|HEX_DIAMOND", "FCC", &S::setInputCrystalStructure),
     opt("--dissolve_small_clusters", "Mark small clusters as OTHER", false, &S::setDissolveSmallClusters),
 };
 
